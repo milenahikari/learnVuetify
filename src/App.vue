fc52c32dac4c3e396f6c5da6da2cb881cddc47a4
Milena Hikari <milenahikari11@gmail.com>
2019-06-23 18:58:42 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,87 @@
 <template>
   <div id="app">
+    <v-toolbar color="teal lighten-1">
+      <v-toolbar-side-icon class="white--text" v-on:click="hide"></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">
+        <router-link to="/">
+          Documentação VUE
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        
+      </v-toolbar-items>
+    </v-toolbar>
+    
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list
+        v-for="(item, index) in items"
+          :key="item.title"  
+      >
+        <v-list-tile @click="toRouter(item.to)">
+          <v-list-tile-action>
+            <v-icon :color="item.color">{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+          
+        </v-list-tile>
+        <v-divider v-if="index + 1 < items.length" :key="`divider-${index}`"></v-divider>
+      </v-list>
+      
+    </v-navigation-drawer>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Index from './components/Pages/Index'
+
+import axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    Index
-  }
+
+  data() {
+    return{
+      titulo:"Aprendendo Diretivas",
+      drawer: false,
+      items: [],
+    }
+  },
+
+  methods: {
+    hide() {
+      this.drawer = !this.drawer;
+    }, 
+    toRouter(url) {
+      this.$router.push(`${url}`);
+    }
+  },
+
+  mounted() {
+    axios.get('/items.json')
+      .then(res => this.items = res.data)
+  },
 }
 </script>
 
 <style>
+#app {
+  font-family: 'Nunito Sans', sans-serif;
+}
+h1 {
+  color: #26A69A;
+  margin-top: 25px;
+  margin-bottom: 15px;
+}
+a {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+}
+p {
+  font-size: 18px;
+}
 </style>
--- src/components/Pages/If.vue
+++ src/components/Pages/If.vue
@@ -1,9 +1,11 @@
 <template>
-  <section>
-    <h2 v-if="condicao">{{texto1}}</h2><!--Se passar false não aparece-->
-    <h2 v-show="condicao">{{texto2}}</h2><!--Se passar false aparece com diplay: none-->
+  <v-container grid-list-md>
+    <h1 v-if="condicao">{{texto1}}</h1><!--Se passar false não aparece-->
+    <p>A diretiva v-if é usada para renderizar condicionalmente um bloco. O bloco só será renderizado se a expressão da diretiva retornar um valor verdadeiro.</p>
+    <p>Assim, se a condição for falsa, o conteúdo HTML não aparece no código.</p>
+    <h1 v-show="condicao">{{texto2}}</h1><!--Se passar false aparece com diplay: none-->
     <router-link to="/"><span>Voltar</span></router-link>
-  </section>
+  </v-container>
 </template>
 
 <script>
